e520aa765eb0890b3cdf1c85e41cb115c60eca42
Endrew <163294939+EndrewTwT@users.noreply.github.com>
2025-06-04 09:14:37 -0300

    1

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Escola',
+    'Para que serve as escolas Cívico Militares?',
+    'objetivo:melhorar a qualidade do ensino e promover um ambiente escolar mais seguro, disciplinado e com foco na formação de cidadãos.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Estado ou Cidade?',
+    'Distrito federal é um estado ou cidade?',
+    'O DF é uma unidade federativa do Brasil, semelhante a um estado, mas sem municípios então ele não é um estado, mas sim uma cidade'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'País',
+    'Qual o melhor pais para se viver?',
+    'A Suíça é consistentemente listada como o melhor país para se viver.'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Qual a diferença entre want e went?',
+    '"Went" significa "fui", "foi", "fomos" ou "foram", no passado do verbo "ir". "Want" significa "querer".'
 )
